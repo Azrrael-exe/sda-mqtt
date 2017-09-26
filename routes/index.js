7ac284328a1327688b2f0476a704aef0ed097a7b
Victor Camargo <vcamargo.e@gmail.com>
2017-09-26 10:28:30 -0500

    Masterkey feature

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,40 @@ var router = express.Router();
 var passport = require('passport')
 var jwt = require('jsonwebtoken');
 var auth = require('../config/auth')
+var User = require('../models/user')
 
 var users = require('./users')
+
+var masterkey = process.env.MASTERKEY || 'Maxwell'
+
+router.post('/signup', User.findUser, function(req, res, next){
+  if(req.body.masterkey == masterkey){
+    User.createUser(req, function(err, newUser){
+      if(err){
+        next(err)
+      }
+      else{
+        newUser.save(function(err){
+          if(err){
+            next(err)
+          }
+          else {
+            res.status(200)
+            res.message = 'User Created'
+            res.payload = newUser
+            next()
+          }
+        })
+      }
+    })
+  }
+  else {
+    var err = new Error('Invalid Masterkey')
+    err.status = 401
+    err.message = 'Invalid Masterkey'
+    return next(err)
+  }
+})
 
 router.post('/auth', passport.authenticate('local-login', {failureRedirect : '/'}), function(req, res){
   var token = jwt.sign(req.user, process.env.TOKEN_SECRET|| 'superSecret', {
@@ -15,13 +47,6 @@ router.post('/auth', passport.authenticate('local-login', {failureRedirect : '/'
   res.json({
     token : token
   })
-})
-
-router.post('/test', function (req, res) {
-  console.log(req.body);
-  console.log(req.headers)
-  res.status(200)
-  res.send('Ok')
 })
 
 router.use('/users', auth.verifyToken, users)
